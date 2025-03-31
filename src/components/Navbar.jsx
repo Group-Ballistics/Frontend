4d14e4166f454481b99bdb84868c9d38056e4458
Yukthi <yukthihettiarachchissck@gmail.com>
2025-03-31 11:27:00 +0530

    start to create nav bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className=''> 
+        <span>
+            <Link to="/" >home</Link>
+        </span>
+    </nav>
+  )
+}
+
+export default Navbar
